eafbc6c59ae7cb70ac27c290cc927b1e3dece3a4
MaeganW <maeganwomble@gmail.com>
2019-02-21 11:38:46 -0600

    added babel es2015 transpiling - ignores test files

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function () {
+  "use strict";
+
+  var random = require('unique-random-array');
+  var fantasyFemaleNames = require('./female-lotr-names.json');
+
+  function returnAllNames() {
+    return fantasyFemaleNames;
+  }
+
+  module.exports = {
+    randomFC: random(fantasyFemaleNames),
+    allFC: returnAllNames
+  };
+})();
