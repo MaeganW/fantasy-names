40ac2d09f4a291cad1b038abe69ae440548bdea7
MaeganW <maeganwomble@gmail.com>
2019-02-22 16:16:43 -0600

    incorporated babel register and istanbul to get nyc to work properly with code coverage on new es6 code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 (function () {
-  "use strict";
-
-  var random = require('unique-random-array');
-  var fantasyFemaleNames = require('./female-lotr-names.json');
+  const random = require('unique-random-array');
+  const fantasyFemaleNames = require('./female-lotr-names.json');
 
   function returnAllNames() {
     return fantasyFemaleNames;
   }
 
   module.exports = {
-    randomFC: random(fantasyFemaleNames),
-    allFC: returnAllNames
+    random: random(fantasyFemaleNames),
+    all: returnAllNames
   }
 })();
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,16 @@
-'use strict';
+const expect = require('chai').expect;
+const fantasyNames = require('./index');
 
-var expect = require('chai').expect;
-var fantasyNames = require('./index');
-
-describe('fantasy-names', function () {
-  it('it should get all the names', function () {
-    var allNames = fantasyNames.allFC();
-    var areAllStrings = allNames.every(function (name) {
+describe('fantasy-names', () => {
+  it('it should get all the names', () => {
+    const allNames = fantasyNames.all();
+    const areAllStrings = allNames.every((name) => {
       return typeof name === 'string';
     });
     expect(areAllStrings).to.be.true;
   });
-  it('it should get a random name', function () {
-    var isAString = typeof fantasyNames.randomFC() === 'string';
+  it('it should get a random name', () => {
+    const isAString = typeof fantasyNames.random() === 'string';
     expect(isAString).to.be.true;
   });
 });
